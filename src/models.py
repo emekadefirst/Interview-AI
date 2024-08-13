773e1e6ec7cb20687d3e2c430fa9c18ba895268b
emekadefirst <victorchibuogwu33@gmail.com>
2024-08-13 08:58:01 +0100

    updated the model and added door.html

--- src/models.py
+++ src/models.py
@@ -1,9 +1,18 @@
 from datetime import datetime
 from sqlmodel import SQLModel, Field
 from database import create_db
+from datetime import datetime
+
+
+def generate_meeting_id():
+    now = datetime.now()
+    date_str = now.strftime('%Y%m%d')
+    time_str = now.strftime('%H%M%S')
+    return f'{date_str}{time_str}'
+
 
 class Applicant(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
+    id: str | None = Field(default=generate_meeting_id, unique=True)
     fullname: str = Field(index=True)
     role: str
     about : str
