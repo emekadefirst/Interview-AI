d5b978b9aae6847965abbb12e712e8547cc4dbe8
emekadefirst <victorchibuogwu33@gmail.com>
2024-08-16 15:35:10 +0100

    ooo

--- src/database.py
+++ src/database.py
@@ -10,9 +10,9 @@ def create_db():
     # if db_url is None:
     #     raise ValueError("PostgreSQL URL not found in environment variables")
     # print(f"Database URL: {db_url}") 
-    # db_name = "ai.db"
-    # db_url = f"sqlite:///{db_name}"  
-    db_url = "postgres://koyeb-adm:GlFrCmZ2k3id@ep-white-cloud-a263j95h.eu-central-1.pg.koyeb.app/koyebdb" 
+    db_name = "ai.db"
+    db_url = f"sqlite:///{db_name}"  
+    # db_url = "postgres://koyeb-adm:GlFrCmZ2k3id@ep-white-cloud-a263j95h.eu-central-1.pg.koyeb.app/koyebdb" 
     engine = create_engine(db_url, echo=True)
     SQLModel.metadata.create_all(engine)
     return engine
